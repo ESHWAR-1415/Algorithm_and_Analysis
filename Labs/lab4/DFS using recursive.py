bfd2ae30ca3828cc0a5947261804d54100dea9ac
ESHWAR-1415 <57348276+ESHWAR-1415@users.noreply.github.com>
2025-09-03 23:43:28 +0530

    Create DFS using recursive.py

---
+++ Labs/lab4/DFS using recursive.py
@@ -0,0 +1,20 @@
+# Implementing dfs using recursively
+def dfs_recursive(graph, node, visited):
+    if node not in visited:
+        print(node, end=" ")   # Process the node
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs_recursive(graph, neighbor, visited)
+
+# Example usage
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+visited = set()
+dfs_recursive(graph, 'A', visited)
